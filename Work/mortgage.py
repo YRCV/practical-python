ed1f60340dac0dc09dc257efa0317a5b8b2fe1ba
YRCV <yrc@njit.edu>
2025-05-21 17:06:46 -0400

    1.8 (extra payment + months)

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,8 +6,13 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0
 months = 0
+extra_payment = 1000
 
 while principal > 0:
+    if months < 12:
+        payment = 2684.11 + extra_payment
+    else:
+        payment = 2684.11
     principal = principal * (1+rate/12) - payment
     total_paid += payment
     months += 1
